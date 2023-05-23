135b60f53b975c5ace6601e8666400c03777a22c
Yurii Rybachok <drummer2009@ukr.net>
2023-05-23 20:01:20 +0300

    login & display user details

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,16 @@
 <script setup>
+import {ref, onMounted } from 'vue';
+import axios from 'axios';
 
+const user = ref();
+
+onMounted(async () => {
+    const data = await axios.get('/api/user');
+    user.value = data.data;
+});
 </script>
 
 <template>
-    
+    <h1>{{ user?.name }}</h1>  
+    <p>{{ user?.email }}</p>  
 </template>
