f36488067fb7808e36d6b929e7d067c854e79326
Yurii Rybachok <drummer2009@ukr.net>
2023-05-23 17:07:47 +0300

    install tailwind & add Login & Register forms

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -13,13 +13,13 @@
         <div class="hidden w-full md:block md:w-auto" id="navbar-default" bis_skin_checked="1">
           <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+              <router-link :to="{name: 'Home'}" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</router-link>
             </li>
             <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</a>
+              <router-link :to="{name: 'Login'}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</router-link>
             </li>
             <li>
-              <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Register</a>
+              <router-link :to="{name: 'Register'}" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Register</router-link>
             </li>
           </ul>
         </div>
--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,17 @@ import Home from '../components/Home.vue';
 const routes = [
     {
         path: '/',
+        name: 'Home',
         component: Home
     },
     {
         path: '/login',
+        name: 'Login',
         component: () => import("../components/Login.vue")
     },
     {
         path: '/register',
+        name: 'Register',
         component: () => import("../components/Register.vue")
     },
 ];
